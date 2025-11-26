a8b7f0f55687098051cd738fbae4c68045a2bed9
thal-carrero <thalia.carrero@icloud.com>
2025-11-26 03:08:37 +0100

    Update index.js with the base requirements

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+let password1El = document.getElementById("password1-el")
+let password2El = document.getElementById("password2-el")
+let passwordLenght = 15
+let password1 = ""
+let password2 = ""
+let isGenerated = false
 
+function getRandomIndex() {
+    return Math.floor(Math.random() * characters.length)
+}
 
+function generate() {
+    if (isGenerated === false) {
+        for (let i = 0; i < passwordLenght ; i++) {
+        password1 += characters[getRandomIndex()]
+        password2 += characters[getRandomIndex()]
+        }
+        password1El.textContent = password1
+        password2El.textContent = password2
+        isGenerated = true
+    } else {
+        password1 = ""
+        password2 = ""
+        isGenerated = false
+        generate()
+    }
+    
+    
+}
 
